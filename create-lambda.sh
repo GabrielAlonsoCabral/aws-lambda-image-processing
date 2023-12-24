38fa851674db1a74d0e61e933dfce5b0fc074787
Gabriel Cabral <gabriel.alonso.dev@icloud.com>
2023-12-23 21:54:56 -0300

    [IMPROVEMENT] - aws-rust-s3-photo-optimization - master

--- create-lambda.sh
+++ create-lambda.sh
@@ -4,4 +4,4 @@ aws lambda create-function --function-name rust-s3-photo-optimization \
      --runtime provided.al2023 \
      --role arn:aws:iam::900909438974:role/lambda-s3-trigger-role \
      --handler rust.handler \
-     --zip-file fileb://target/lambda/rust-s3-photo-optimization/bootstrap.zip
+     --zip-file fileb://target/lambda/aws-rust-image-processing/bootstrap.zip
